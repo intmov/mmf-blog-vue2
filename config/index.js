15916dcfb3524099dacc6fe52a644fdfc739c701
intmov <intmov@qq.com>
2018-03-30 10:30:13 +0800

    优化缓存

--- config/index.js
+++ config/index.js
@@ -13,7 +13,9 @@ module.exports = {
         // Surge or Netlify already gzip all static assets for you.
         // Before setting to `true`, make sure to:
         // npm install --save-dev compression-webpack-plugin
-        productionGzip: false,
+        productionGzip: true,
+        productionGzipExtensions: ['js', 'css'],
+
         proxyTable: {
             '/api': {
                 target: 'http://localhost:4000',
@@ -22,9 +24,8 @@ module.exports = {
                     '^/api': '/api'
                 }
             }
-        },
+        }
 
-        productionGzipExtensions: ['js', 'css']
     },
     dev: {
         env: require('./dev.env'),
